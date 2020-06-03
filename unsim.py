fee74f645854422bee6c5bd15ab13826fdc9cfb5
ojgurney-champion <oliverchampion@gmail.com>
2020-06-03 18:25:23 +0200

    print lr at end

--- unsim.py
+++ unsim.py
@@ -15,11 +15,12 @@ b=np.array([0, 5, 10, 20, 30, 40, 60, 150, 300, 500, 700])
 
 matlsq = np.zeros([len(SNR)*len(lrs), 3, 3])
 matNN = np.zeros([len(SNR)*len(lrs), 3, 3])
+stability = np.zeros([len(SNR)*len(lrs), 3])
 a = 0
 for arg.lr in lrs:
     for aa in SNR:
         print('\n simulation at SNR of {snr}\n'.format(snr=aa))
-        matlsq[a, :, :],matNN[a, :, :], stability = sim.sim(aa, b, arg)
+        matlsq[a, :, :],matNN[a, :, :], stability[a,:] = sim.sim(aa, b, arg)
         a = a + 1
         print('\nresults from lsq:')
         print(matlsq)
@@ -28,3 +29,15 @@ for arg.lr in lrs:
         if arg.repeats > 1:
             print('\nstability of NN:')
             print(stability)
+a = 0
+for arg.lr in lrs:
+    for aa in SNR:
+        print('\nlearning rate is:{}'.format(arg.lr))
+        print('\nresults from lsq:')
+        print(matlsq[a])
+        print('\nresults from NN:')
+        print(matNN[a])
+        if arg.repeats > 1:
+            print('\nstability of NN:')
+            print(stability[a])
+        a = a + 1
